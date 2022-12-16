2b96b51c18d36d51170782417f28949d5adbcaeb
programadoraelaine@gmail.com <programadoraelaine@gmail.com>
2022-12-16 01:12:27 +0000

    LEFT+RIGHT+INNER

---
+++ JOIN.sql
@@ -0,0 +1,11 @@
+-- Databricks notebook source
+--LEFT JOIN--
+
+SELECT T1.*,
+       T2.descUF
+
+FROM silver_olist.pedido AS T1
+
+LEFT JOIN silver_olist.cliente AS T2
+ON T1.idCliente = T2.idCliente
+
